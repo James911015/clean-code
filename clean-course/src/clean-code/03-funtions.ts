c92f5eb4eabcd2ad22860539496d2c8debcc69f8
James911015 <32137620+James911015@users.noreply.github.com>
2025-08-23 11:51:03 -0500

    Ejercicios funciones

---
+++ clean-course/src/clean-code/03-funtions.ts
@@ -0,0 +1,39 @@
+(() => {
+
+    // función para obtener información de una película por Id
+    function getMovieById( id: string ) {
+        console.log({ id });
+    }
+
+    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+    function getMovieCastById( id: string ) {
+        console.log({ id });
+    }
+
+    // funcion para obtener el bio del actor por el id
+    function getBiographyActorById( id: string ) {
+        console.log({ id });
+    }
+    
+    // Crear una película
+    function createMovie(title: string, description: string, rating: number, cast: string[] ) {
+        console.log({ title, description, rating, cast });
+    }
+
+    // Crea un nuevo actor
+    function createActor( fullName: string, birthdate: Date ): boolean {
+        
+        // tarea asincrona para verificar nombre
+        // ..
+        // ..
+        if ( fullName === 'fernando' ) return false;
+
+        console.log('Crear actor');
+        return true;        
+
+    }
+
+    
+
+
+})();
